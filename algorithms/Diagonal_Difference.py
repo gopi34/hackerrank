d66a57d8d70777fc96d256815c5816c0f913c10a
gopi34 <gopinath.livewire@gmail.com>
2017-03-30 21:06:34 +0530

    Create Diagonal_Difference.py

---
+++ algorithms/Diagonal_Difference.py
@@ -0,0 +1,19 @@
+Sample Output
+
+15
+
+Explanation
+
+The primary diagonal is:
+11
+      5
+            -12
+
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+            4
+      5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
